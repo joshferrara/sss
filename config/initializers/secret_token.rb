3a916e62fa80aa2bd6b42ccda1beef36f813e7f1
Josh Ferrara <josh@hivemindlabs.com>
2012-12-02 20:12:50 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sss::Application.config.secret_token = '9d87e44b1ede2aebd42c1ec4cd1386c340e42e8a3bdd9d641575f636f84cfbcf950f86d7b5c5c381062ca0f36215079a77b67c491ff64fc67dad8edec1ee3a5b'
